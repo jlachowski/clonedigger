3f7b024ccb755cc1b47ca9be9938b7c33e60ed4d
Jaroslaw Lachowski <jaroslaw.lachowski@wilabs.com>
2014-01-31 11:33:30 +0100

    added base exception class

--- clonedigger/java_antlr.py
+++ clonedigger/java_antlr.py
@@ -21,18 +21,22 @@ import xml.parsers.expat
 
 from abstract_syntax_tree import *
 
+
 class JavaANTLRSourceFile (SourceFile):
     extension = 'java'
     size_threshold = 10
     distance_threshold = 7
+
     def __init__(self, file_name):
         SourceFile.__init__(self, file_name)
+
         class ExpatHandler:
             def __init__(self, start_node, parent):
                 self.parent = parent
                 self.stack = [start_node]
+
             def start_element(expat_self, xml_node_name, attrs):
-                line_number = int(attrs["line_number"])-1
+                line_number = int(attrs["line_number"]) - 1
                 line_numbers = [line_number]
                 if line_numbers == [-1]:
                     line_numbers = []
@@ -44,13 +48,16 @@ class JavaANTLRSourceFile (SourceFile):
                     assert(xml_node_name == "node")
                 expat_self.stack[-1].addChild(r)
                 expat_self.stack.append(r)
+
             def end_element(self, name):
                 self.stack.pop()
 
-        tree_file_name  = 'temporary_ast.xml'
+        tree_file_name = 'temporary_ast.xml'
         current_directory = os.path.realpath(os.path.dirname(__file__))
-        producer_class_path = os.path.join(current_directory,'java_antlr', 'TreeProducer.jar')
-        antlr_class_path = os.path.join(current_directory, 'antlr_runtime', 'runtime-2008-01-10.16.jar')
+        producer_class_path = os.path.join(
+            current_directory, 'java_antlr', 'TreeProducer.jar')
+        antlr_class_path = os.path.join(
+            current_directory, 'antlr_runtime', 'runtime-2008-01-10.16.jar')
         if os.name in ['mac', 'posix']:
             class_path_delimeter = ':'
         elif os.name in ['nt', 'dos', 'ce']:
@@ -58,11 +65,15 @@ class JavaANTLRSourceFile (SourceFile):
         else:
             print 'unsupported OS'
             assert(0)
-        if os.system('java -classpath ' + producer_class_path + class_path_delimeter + antlr_class_path + ' TreeProducer %s %s 2>err.log' %(file_name, tree_file_name)):
+        command = (
+            'java -classpath ' + producer_class_path
+            + class_path_delimeter + antlr_class_path
+            + ' TreeProducer %s %s 2>err.log' % (file_name, tree_file_name))
+        if os.system(command):
             f = open('err.log')
             s = f.read()
             f.close()
-            raise s
+            raise Exception(s)
 
         self._tree = AbstractSyntaxTree('program')
         handler = ExpatHandler(self._tree, self)
