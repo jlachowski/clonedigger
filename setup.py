17835ef81f0cd30f494acc5c2754974be9a412b5
Jaroslaw Lachowski <jaroslaw.lachowski@wilabs.com>
2015-06-25 19:09:10 +0200

    future requirement added

--- setup.py
+++ setup.py
@@ -47,6 +47,6 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
-    install_requires=['setuptools', 'future'],
+    install_requires=['future', 'setuptools'],
     entry_points=entry_points,
 )
